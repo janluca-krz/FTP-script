0704a5405b56cbcd59a02a7449ffbfd714b18680
Janluca-krz <xmodie.ttv@gmail.com>
2023-09-21 10:05:05 +0200

    Checked Functionality

--- index.py
+++ index.py
@@ -2,13 +2,15 @@ import ftplib
 import pandas as pd
 
 # FTP-Zugangsdaten
-ftp_host = 'Server'
-ftp_user = 'benutzername'
-ftp_pass = 'pass'
+ftp_host = 'ek-product-data.ek-fileserver.de'
+ftp_user = 'compravo'
+ftp_pass = 'Ue76!3hh'
 
-ftp_host2 = 'Server'
-ftp_user2 = 'benutzername'
-ftp_pass2 = 'pass'
+ftp_host2 = 'sftp3.inwebs.com'
+ftp_port2 = 12022
+ftp_dir2 = '/40010/out/Bestand'
+ftp_user2 = 'compravo'
+ftp_pass2 = 'Adk!2390AmLopEzt!23'
 
 # Dateinamen
 remote_filename = 'EK_stock.csv'
@@ -18,41 +20,34 @@ local_filename = 'BESTAND_EK.csv'
 ftp = ftplib.FTP(ftp_host)
 ftp.login(ftp_user, ftp_pass)
 
-ftp2 = ftplib.FTP(ftp_host2)
-ftp2.login(ftp_user2, ftp_pass2)
-
 # CSV-Datei herunterladen
 with open(local_filename, 'wb') as local_file:
     ftp.retrbinary('RETR ' + remote_filename, local_file.write)
 
+# Verbindung zum zweiten FTP-Server herstellen
+ftp2 = ftplib.FTP_TLS(ftp_host2, ftp_user2, ftp_pass2)
+ftp2.cwd(ftp_dir2)
+
 # CSV-Datei transformieren
-df = pd.read_csv(local_filename)
-df['H'] = df['']
-df['INVRPT'] = df['']
-df['Debitor'] = df['']
-df['GLN'] = df['']
-df['DatRpt'] = df['']
+df = pd.read_csv(local_filename, sep=',')
+print(df.columns)
 
-# Umbenennen der Spalte "EAN" in "EK Artikelnummer"
+# Hier füge die entsprechenden Zuordnungen der Spalten ein
+df['H;'] = ''
+df['INVRPT;'] = ''
+df['Debitor;'] = ''
+df['GLN;'] = ''
+df['DatRpt;'] = ''
+
 df.rename(columns={'EAN': 'EK Artikelnummer'}, inplace=True)
-df['EK Artikelnummer'] = local_filename['EK Artikelnummer']
-
-# Umbenennen der Spalte "Artikel_Nr" in "EANUPC"
 df.rename(columns={'Artikel_Nr': 'EANUPC'}, inplace=True)
-df['EANUPC'] = local_filename['EANUPC']
-df['EANTyp'] = df['']
-df['Verf'] = df['']
-df['KZ-Planlieferz'] = df['']
-df['H'] = df['']
-
 
 # Transformierte CSV-Datei lokal speichern
-transformed_filename = 'BESTAND_EK.csv'
+transformed_filename = 'BESTAND_EK_transformed.csv'
 df.to_csv(transformed_filename, index=False)
 
-# Transformierte CSV-Datei auf den FTP-Server hochladen
+# Transformierte CSV-Datei auf den zweiten FTP-Server hochladen
 with open(transformed_filename, 'rb') as transformed_file:
-
     ftp2.storbinary('STOR ' + transformed_filename, transformed_file)
 
 # Verbindung zum FTP-Server schließen
